84c115523936dc389d3c24bf7f54ea2feb9d3775
Aman <aman.singh@cynoteck.com>
2024-01-05 13:25:32 +0530

    theme dark light

--- app/src/main/java/com/petofy/androidarchdemoprojects/theme/ThemeActivity.kt
+++ app/src/main/java/com/petofy/androidarchdemoprojects/theme/ThemeActivity.kt
@@ -2,12 +2,36 @@ package com.petofy.androidarchdemoprojects.theme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CompoundButton
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatDelegate
 import com.petofy.androidarchdemoprojects.R
+import com.petofy.androidarchdemoprojects.databinding.ActivityThemeBinding
 
 class ThemeActivity : AppCompatActivity() {
+    lateinit var binding: ActivityThemeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_theme)
+        binding = ActivityThemeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        /*
+        * isEnabled controls whether the switch can be interacted with.
+        * isActivated controls the visual appearance of the switch, indicating its "active" state.
+        * isChecked represents the actual on/off state of the switch.
+        * */
+        binding.switch1.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                Toast.makeText(this, "Night Mode ON!", Toast.LENGTH_SHORT).show()
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+//        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
+
+
 
     }
 }
